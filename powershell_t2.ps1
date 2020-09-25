ca3cecc273e191138e902ae2fd1a45f63d19389a
Bhavin <bhavinvadher80@gmail.com>
2020-09-25 16:53:21 +0100

    Updated file path

--- powershell_t2.ps1
+++ powershell_t2.ps1
@@ -1,4 +1,4 @@
-#Q1) $users = Import-Csv -Path F:\Jobs\TakeTwo\Users.csv
+#Q1) $users = Import-Csv -Path F:\TakeTwo\Users.csv
 #Q2) $users | Measure-Object -Property Emailaddress
 #Q3) $users | Measure-Object -Sum MailboxSizeGB
 #Q4) $users | Where-Object {$_.Emailaddress -cnotmatch $_.UserPrincipalName}
